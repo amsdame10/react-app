7f0a5f9c874b7cae095b570dede57038cefba2dc
amsdame10 <122823920+amsdame10@users.noreply.github.com>
2023-04-02 20:18:21 -0700

    Changed css and updated app.js semicolon

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ function App() {
         <h1> Weather Search Engine!</h1>
         <Weather />
       </header>
+      <footer>
+        This project was coded by Andrea Smith and is open-sourced on{" "}
+        <a
+          href="https://github.com/amsdame10/react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub{" "}
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://deft-sherbet-f9cb0a.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify{" "}
+        </a>
+      </footer>
     </div>
   );
 }

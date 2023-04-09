646321300651bcf0d1504b1a79a484323b347909
amsdame10 <122823920+amsdame10@users.noreply.github.com>
2023-04-08 21:00:13 -0700

    Added Weather js and removed

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-
 export default function Weather() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
